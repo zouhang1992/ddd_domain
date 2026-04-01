a62abaef93bbf146b9d24c14b6b2c130aed1e2f2
zouhang.orange <zouhang.orange@bytedance.com>
2026-04-01 15:24:03 +0800

    refactor: remove old auth middleware references

--- internal/application/common/module.go
+++ internal/application/common/module.go
@@ -8,14 +8,10 @@ import (
 	locationrepo "github.com/zouhang1992/ddd_domain/internal/domain/location/repository"
 	landlordrepo "github.com/zouhang1992/ddd_domain/internal/domain/landlord/repository"
 	"go.uber.org/fx"
-	"time"
 )
 
 // Module provides common application components
 var Module = fx.Options(
-	fx.Provide(func() *service.AuthService {
-		return service.NewAuthService("", 7*24*time.Hour)
-	}),
 	fx.Provide(func(billRepo billrepo.BillRepository, leaseRepo leaserepo.LeaseRepository, roomRepo roomrepo.RoomRepository, locationRepo locationrepo.LocationRepository, landlordRepo landlordrepo.LandlordRepository) *service.PrintService {
 		return service.NewPrintService(billRepo, leaseRepo, roomRepo, locationRepo, landlordRepo)
 	}),
